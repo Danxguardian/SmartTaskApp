c5d0667c58056cfa43c08e4d3cdd0f3e2138b683
Danzero <danxguardian@gmail.com>
2021-11-24 01:33:59 -0600

    FEAT: Add loader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import React, { useState, useEffect } from "react";
-
+import { useDispatch, useSelector } from "react-redux";
+import { CLoader } from "./01_Components/CLoader/index";
 /* import logo192 from "../public/src/logo192.png";
 import logo512 from "../public/src/logo512.png"; */
 
+import { getInfoRequest } from "./05_Store/Actions/AInformation";
+import { showLoader } from "./05_Store/Actions/ALoader";
 /* implementar route guard */
-function App() {
-	return (
-		<div className="container col">
-			<div className="row justify-content-md-center">
-				<div className="testBorder col-12 col-lg-2">SideBar </div>
-				<div className="testBorder col-12 col-lg-10">Content</div>
-			</div>
-		</div>
-	);
-}
+export const App = () => {
+	const dispatch = useDispatch();
+	/* dispatch(getInfoRequest({})); */
+	/* implementar action for dialogue errors */
 
-export default App;
+	const loading = useSelector((state) => state.loader);
+
+	useEffect(() => {
+		dispatch(showLoader());
+	}, []);
+
+	return (
+		<>
+			<div className="container col">
+				<div className="row justify-content-md-center">
+					<div className="testBorder col-12 col-lg-2">SideBar </div>
+					<div className="testBorder col-12 col-lg-10">Content</div>
+				</div>
+			</div>
+
+			{loading.status && <CLoader title={loading.title} />}
+			{/* implementar dialogue errors */}
+		</>
+	);
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import "./assets/css/styles.css";
+import { Provider } from "react-redux";
+import { App } from "./App";
+import store from "./05_Store/index";
+import "./assets/css/allStyles.css";
 
 const target = document.getElementById("root");
-ReactDOM.render(<App />, target);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	target
+);
 if (module.hot) {
 	module.hot.accept();
 }
